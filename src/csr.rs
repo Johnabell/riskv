a2367812df9f12e3bcd3ac7e971356ff53d22e16
John Bell <bell.john.andrew@gmail.com>
2023-06-30 23:34:20 +0100

    Add documentation about the control status registers

--- src/csr.rs
+++ src/csr.rs
@@ -1,19 +1,66 @@
 use std::sync::atomic::{AtomicI32, AtomicI64, Ordering::SeqCst};
 
+/// According to the RISC-V specification, the number of control status registers.
 const CSR_SIZE: usize = 4096;
 
+/// The control status registers.
 pub trait CSR {
     type Register;
+    /// Swaps the value for the value at `index`.
+    ///
+    /// Panics if index is out of bounds (>`CSR_SIZE`)
     fn read_write(&self, index: u16, value: Self::Register) -> Self::Register;
+    /// Set the bits in the `value` bit mask.
+    ///
+    /// Panics if index is out of bounds (>`CSR_SIZE`)
     fn set_bits(&self, index: u16, value: Self::Register) -> Self::Register;
+    /// Clears the bits in the `value` bit mask.
+    ///
+    /// Panics if index is out of bounds (>`CSR_SIZE`)
     fn clear_bits(&self, index: u16, value: Self::Register) -> Self::Register;
 }
 
+/// The 32-bit control status registers.
+///
+/// This is currently a naïve implementation which simply is a heap allocated
+/// fixed size array.
+///
+/// For example, the following instructions represent reading certain system
+/// attributes from the CSRs
+/// - `RDCYCLE[H]` - a pseudo-instruction to read the cycle CSR which holds the
+///   count of the number of clock cycles executed by the processor core on
+///   which the hart is running form an arbitrary time in the past
+/// - `RDTIME[H]` - a pseudo-instruction to read the time CSR which counts the
+///   wall-clock real time that has passed from an arbitrary start time in the
+///   past.
+/// - `RDINSTRET[H]` - a pseudo-instruction to read the instret CSR which counts
+///   the number of instructions retired by this hart from some arbitrary start
+///   point in the past.
+///
+/// Additionally some registers are read only.
 #[derive(Debug)]
 pub struct CSR32 {
     registers: Vec<AtomicI32>,
 }
 
+/// The 64-bit control status registers.
+///
+/// This is currently a naïve implementation which simply is a heap allocated
+/// fixed size array.
+///
+/// For example, the following instructions represent reading certain system
+/// attributes from the CSRs
+/// - `RDCYCLE` - a pseudo-instruction to read the cycle CSR which holds the
+///   count of the number of clock cycles executed by the processor core on
+///   which the hart is running form an arbitrary time in the past
+/// - `RDTIME` - a pseudo-instruction to read the time CSR which counts the
+///   wall-clock real time that has passed from an arbitrary start time in the
+///   past.
+/// - `RDINSTRET` - a pseudo-instruction to read the instret CSR which counts
+///   the number of instructions retired by this hart from some arbitrary start
+///   point in the past.
+///
+/// Additionally some registers are read only.
 #[derive(Debug)]
 pub struct CSR64 {
     registers: Vec<AtomicI64>,
